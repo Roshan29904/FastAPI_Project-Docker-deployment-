2d9b1f779bdea60bedba3650f127a417baf16370
Roshan29904 <roshan.rai29904@gmail.com>
2026-07-10 01:57:20 +0530

    chaanged made

--- model/predict.py
+++ model/predict.py
@@ -11,8 +11,26 @@ with open("model/model.pkl", "rb") as f:
 #model version --> this usually comes from MLFlow
 MODEL_VERSION = "1.0.0"
 
+# Getting class labels from model  this is important for matching probabilities to class names
+class_labels = model.classes_tolist()
+
 
 def predict_output(user_input: dict):
+    #creating data frame
     df = pd.DataFrame([user_input])
-    output = model.predict(df)[0]
-    return output
+    
+    #predict the class
+    predicted_class = model.predict(df)[0]
+    
+    # gettng proba for all classes
+    probailities = model.predict_proba(df)[0]
+    confidence = max(probailities)
+    
+    #creating map -> {class_name: proba}
+    class_proba = dict(zip(class_labels, map(lambda p: round(p,4), probailities)))
+    
+    return {
+        "predicted_category": predicted_class,
+        'confidence': round(confidence, 4),
+        "class_probabilities": class_proba
+    }
